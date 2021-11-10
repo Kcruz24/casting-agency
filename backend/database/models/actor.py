eb1b90800f66aef70904bae4a374aff85d9ad884
Kevin Cruz <kocruz.24@gmail.com>
2021-11-10 11:07:03 -0400

    Add Actor model

---
+++ backend/database/models/actor.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+
+db = SQLAlchemy()
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def update():
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+    def __repr__(self):
+        return f'<id: {self.id}, ' \
+               f'name: {self.name}, ' \
+               f'age: {self.age}, ' \
+               f'gender: {self.gender}>'
