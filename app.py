c917519ac93fd0c7bb2f334262cb2e712f23845e
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-05 22:10:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -281,3 +281,8 @@ def create_app(test_config=None):
         }), error.status_code
 
     return app
+
+
+app = create_app()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
