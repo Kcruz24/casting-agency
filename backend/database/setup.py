4009b1f04ac51a2d164a83c70c94e692a2142c03
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-05 22:19:48 -0400

    Update setup.py

--- backend/database/setup.py
+++ backend/database/setup.py
@@ -13,12 +13,16 @@ database_host = 'localhost:5432'
 #                                                   database_host,
 #                                                   database_name)
 
-database_path = config('DATABASE_URL')
+db_uri = config('DATABASE_URL')
+if db_uri.startswith('postgres://'):
+    db_uri = db_uri.replace('postgres://', 'postgresql://', 1)
+
+
 db = SQLAlchemy()
 SECRET_KEY = os.urandom(32)
 
 
-def setup_db(app, db_path=database_path):
+def setup_db(app, db_path=db_uri):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config['SECRET_KEY'] = SECRET_KEY
