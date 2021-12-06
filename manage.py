9dad7a6803067e7914115b4f0b64a98f9bd8365d
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-05 21:05:13 -0400

    Add manage.py

---
+++ manage.py
@@ -0,0 +1,14 @@
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+import app
+from backend.database.setup import db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
