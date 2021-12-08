b86bf49e8c2f89ec0573efb32df16a5c2666f6b4
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-08 11:53:06 -0400

    Delete Flask-WTF forms

--- backend/forms/movie_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, DateField
-from wtforms.validators import DataRequired
-
-
-class MovieForm(FlaskForm):
-    title = StringField('title', validators=[DataRequired()])
-    release_date = DateField('release_date', validators=[DataRequired()])
