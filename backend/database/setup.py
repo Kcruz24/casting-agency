c36f822270eb906a99530d9702ab3fb636382019
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-08 11:54:23 -0400

    Update setup.py to accord the documentation

--- backend/database/setup.py
+++ backend/database/setup.py
@@ -3,20 +3,9 @@ import os
 from decouple import config
 from flask_sqlalchemy import SQLAlchemy
 
-database_username = 'postgres'
-database_password = config('PASSWORD')
-database_name = 'casting_agency_test'
-database_host = 'localhost:5432'
-
-# database_path = "postgresql://{}:{}@{}/{}".format(database_username,
-#                                                   database_password,
-#                                                   database_host,
-#                                                   database_name)
-
 db_uri = config('DATABASE_URL')
 if db_uri.startswith('postgres://'):
     db_uri = db_uri.replace('postgres://', 'postgresql://', 1)
-
 
 db = SQLAlchemy()
 SECRET_KEY = os.urandom(32)
